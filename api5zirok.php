421c1861657b063406e67937e5a84dcd89ca4df6
lkrasnoselska <36417808+lkrasnoselska@users.noreply.github.com>
2018-04-02 21:58:18 +0300

    nab

---
+++ api5zirok.php
@@ -0,0 +1,20 @@
+//$sql = "SELECT *  FROM `SS_products` WHERE `enabled` = 1"; //Выборка номеров товара
+
+<?php
+//Подключение библиотек
+	include("./cfg/".$_SERVER["SERVER_NAME"].".connect.inc.php");
+   include("./cfg/".$_SERVER["SERVER_NAME"].".cfg.php");
+  require_once('./error_log.php');
+  require_once("./core_functions/product_functions.php");
+  require_once("./core_functions/functions.php");
+  require_once("./core_functions/datetime_functions.php");
+//authorized login check
+   include("./checklogin.php");
+   
+//Авторизация на сервере
+//$sql = "SELECT * FROM `users`"; // выборка пользователей
+//$sql = "SELECT *  FROM `SS_customers` WHERE `Login` LIKE \'andrey_r\'"; //Определение пароля по Login
+$responce = db_fetch_row(db_query("SELECT * FROM `users`"));
+
+print json_encode($responce);
+?>
